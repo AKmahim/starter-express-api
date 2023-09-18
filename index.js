13053595b0d0b3cf7de71c0fdbdfdec948d7bc8c
AKmahim <mdmahimhossen134@gmail.com>
2023-09-19 02:00:04 +0600

    AR photoBooth api

--- index.js
+++ index.js
@@ -1,7 +1,54 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require('express');
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const port = 3000;
+
+// Create a directory to store uploaded images
+const uploadDir = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
+// Multer configuration for handling file uploads
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    const fileId = uuidv4();
+    const fileExtension = path.extname(file.originalname);
+    cb(null, `${fileId}${fileExtension}`);
+  },
+});
+
+const upload = multer({ storage });
+
+// Define a route for uploading an image
+app.post('/upload', upload.single('image'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ message: 'No file uploaded.' });
+  }
+
+  const fileId = path.basename(req.file.filename);
+  return res.json({ success: true, id: fileId });
+});
+
+// Define a route for retrieving an image by ID
+app.get('/image/:id', (req, res) => {
+  const fileId = req.params.id;
+  const filePath = path.join(uploadDir, fileId);
+
+  if (fs.existsSync(filePath)) {
+    return res.sendFile(filePath);
+  } else {
+    return res.status(404).json({ message: 'Image not found.' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
